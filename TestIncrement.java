80e6a2bba7c7625b651925707c142bf5811ff1f7
thiriss <thiriss999@gmail.com>
2023-01-11 18:37:07 +0630

    feat: increment exercise

--- TestIncrement.java
+++ TestIncrement.java
@@ -4,17 +4,19 @@ public class TestIncrement {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i =10; 
-		// assigned 10 to i
-		int ilnc = i++;
-		//increment after operation
+		// variable declaration
+		int i,ilnc; 
+		
+		i = 10; 
+		ilnc = i++;
+		//postfix increment	
+		System.out.println("The value of ilnc = " + ilnc);
 		System.out.println("The value of i = " + i);
-		System.out.println("The value of postfix ilnc = " + ilnc);
 		
 		ilnc = ++i;
-		//increment before operation
+		//prefix increment
+		System.out.println("The value of ilnc = " + ilnc);
 		System.out.println("The value of i = " + i);
-		System.out.println("The value of prefix ilnc = " + ilnc);
 		
 	}
 
